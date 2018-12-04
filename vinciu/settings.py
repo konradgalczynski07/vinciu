c70d855c042c75eedf3a754ca18e2b2741ca4875
konradgalczynski07 <konradgalczynski07@gmail.com>
2018-12-04 23:14:44 +0100

    basic templates and routes added

--- vinciu/settings.py
+++ vinciu/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'vinciu/staticfiles')
+    os.path.join(BASE_DIR, 'vinciu/static')
 ]
 
 
---
+++ vinciu/static/js/main.js
@@ -0,0 +1,2 @@
+// Get the current year for the copyright
+$('#year').text(new Date().getFullYear());
