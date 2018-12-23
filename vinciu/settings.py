84ea1929f944390407837d8b0129ad5712448105
konradgalczynski07 <konradgalczynski07@gmail.com>
2018-12-23 21:39:04 +0100

    deloying to heroku

--- vinciu/settings.py
+++ vinciu/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
-import os
 import django_heroku 
+import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
